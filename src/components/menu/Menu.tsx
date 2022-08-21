fc2083f6c7f2c3173ee4a46408b55997128f6e83
Jordy Pena <67558072+JordyPena@users.noreply.github.com>
2022-08-21 16:29:02 -0500

    Home finalize page (#2)
    * home finalized
    
    * added border

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -7,62 +7,100 @@ import { useState } from "react";
 import { Link } from 'react-router-dom';
 import "../menu/Menu.css";
 const Menu = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [sidebar, setSidebar] = useState(false);
 
-  const menuClicked = () => {
-    setIsMenuOpen(!isMenuOpen);
+  const showSidebar = () => {
+    setSidebar(!sidebar);
   };
   return (
-    <nav className="navBar">
-      {!isMenuOpen && (
-        <button onClick={menuClicked}>{<HiMenu size="25px" />}</button>
-      )}
-      <div className={`menuNav ${isMenuOpen && "showMenu"}`}>
-        <span className="close" onClick={menuClicked}>
-          <AiOutlineClose size="25px" />
-        </span>
-        <div className="menuItems">
-          <ul>
-            <li>
-              <Link to="/" style={{textDecoration: 'none'}}>
-                <FaHome size="25px" />
-                <span>Home</span>
-              </Link>
-            </li>
-            <li>
-              <Link to="/special" style={{textDecoration: 'none'}}>
-                <BsFillStarFill size="25px" />
-                <span>Special</span>
-              </Link>
-            </li>
-            <li>
-              <Link to="/liveShows" style={{textDecoration: 'none'}}>
-                <GiTicket size="25px" />
-                <span>Live Shows</span>
-              </Link>
-            </li>
-            <li>
-              <a target="_blank" href="https://www.patreon.com/AreYouGarbage">
-                <FaPatreon size="25px" />
-                <span>Patreon</span>
-              </a>
-            </li>
-            <li>
-              <a target="_blank" href="https://www.youtube.com/c/AreYouGarbageComedyPodcast/featured">
-                <FaYoutube size="25px" />
-                <span>Youtube</span>
-              </a>
-            </li>
-            <li>
-              <a target="_blank" href='https://www.bonfire.com/store/are-you-garbage/'>
-                <FaTshirt size="25px" />
-                <span>Merch</span>
-              </a>
-            </li>
-          </ul>
+    <>
+      <div className="navbar">
+        <div className="left-container">
+          <img
+            src={`${process.env.PUBLIC_URL}/images/banner.jpg`}
+            className="nav-img"
+            alt="two guys"
+          />
+          {/* <p className="left-container-text">Are You Garbage</p> */}
         </div>
+
+        {!sidebar ? (
+          <Link to="#" className="menu-bars" onClick={showSidebar}>
+            <HiMenu className="menu-bars-icon"/>
+          </Link>
+        ) : (
+          <Link to="#" className="menu-bars" onClick={showSidebar}>
+            <AiOutlineClose className="menu-bars-icon"/>
+          </Link>
+        )}
       </div>
-    </nav>
+      <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+        <ul className="nav-menu-items">
+          {/* TODO: maybe add a searchbar see code below */}
+          {/* {renderProps.location.pathname === "/" ? (
+            ""
+          ) : errorMessage === true ? (
+            <p className="errorMessage">
+              Can only search by Pokémon name ie: blastoise
+            </p>
+          ) : (
+            ""
+          )} */}
+
+          {/* <li className="searchBar">
+            {renderProps.location.pathname === "/" ? "" : searchBar}
+          </li> */}
+          <li>
+            <img
+              src={`${process.env.PUBLIC_URL}/images/titleLogo.png`}
+              className="nav-menu-img"
+              alt="Are You Garbage?"
+            />
+          </li>
+          <li className="nav-text" onClick={showSidebar}>
+            <Link to="/">
+              <FaHome className="link-style" />{" "}
+              <p className="dark-mode-style">Home</p>
+            </Link>
+          </li>
+          <li className="nav-text" onClick={showSidebar}>
+            <Link to="/special">
+              <BsFillStarFill className="link-style" />{" "}
+              <p className="dark-mode-style">Special</p>
+            </Link>
+          </li>
+
+          <li className="nav-text" onClick={showSidebar}>
+            <Link to="/liveShows">
+              <GiTicket className="link-style" />{" "}
+              <p className="dark-mode-style">Live Shows</p>
+            </Link>
+          </li>
+          <li className="nav-text" onClick={showSidebar}>
+            <a target="_blank" href="https://www.patreon.com/AreYouGarbage">
+              <FaPatreon className="link-style" />{" "}
+              <p className="dark-mode-style">Patreon</p>
+            </a>
+          </li>
+          <li className="nav-text" onClick={showSidebar}>
+            <a target="_blank" href="https://www.youtube.com/c/AreYouGarbageComedyPodcast/featured">
+              <FaYoutube className="link-style" />{" "}
+              <p className="dark-mode-style">Episodes</p>
+            </a>
+          </li>
+          <li className="nav-text" onClick={showSidebar}>
+            <a target="_blank" href="https://www.bonfire.com/store/are-you-garbage/">
+              <FaTshirt className="link-style" />{" "}
+              <p className="dark-mode-style">Merch</p>
+            </a>
+          </li>
+          {/* TODO: add theme see code below */}
+          {/* <li className="nav-text" onClick={showSidebar}>
+            <Themes />
+          </li> */}
+        </ul>
+      </nav>
+    </>
   );
 };
 export default Menu;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,94 @@
-import Topnav from "../../components/top-nav/TopNav";
-import Menu from '../../components/menu/Menu';
+import Menu from "../../components/menu/Menu";
+import "../home/Home.css";
 const Home = () => {
   return (
     <>
-      <Topnav/>
-      <Menu/>
+      <Menu />
+      <section className="iframe-Container">
+        <h1 className="title">AYG Comedy Special</h1>
+        <iframe
+          title="Are You Garbage Special"
+          src="https://www.youtube.com/embed/jStGYCXXHXA"
+          height="350"
+          width="350"
+        ></iframe>
+        <div className="wave">
+          <svg
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+              className="shape-fill"
+            ></path>
+          </svg>
+        </div>
+      </section>
+      <section className="listen-section-container">
+        <h3 className="title-two">Are You Garbage</h3>
+        <h2 className="subtitle">Show available on</h2>
+        <div className="platform-link-container">
+          <div className="link-background">
+            <a target="_blank" href="https://www.patreon.com/AreYouGarbage">
+              <figure>
+                <span>
+                  <img
+                    className="platform-link-styles"
+                    src={`${process.env.PUBLIC_URL}/images/patreon.png`}
+                    alt="Patreon"
+                  />
+                </span>
+              </figure>
+            </a>
+            <a
+              target="_blank"
+              href="https://www.youtube.com/c/AreYouGarbageComedyPodcast"
+            >
+              <figure>
+                <span>
+                  <img
+                    className="platform-link-styles"
+                    src="https://fluxconsole.com/files/image/189425?width=375"
+                    alt="Spotify"
+                  />
+                </span>
+              </figure>
+            </a>
+            <a
+              target="_blank"
+              href="https://open.spotify.com/show/35Ntc5kndMt5uUCD3RtLhL"
+            >
+              <figure>
+                <span>
+                  <img
+                    className="platform-link-styles"
+                    src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png"
+                    alt="Spotify"
+                  />
+                </span>
+              </figure>
+            </a>
+            <a
+              target="_blank"
+              href="https://podcasts.apple.com/us/podcast/are-you-garbage-comedy-podcast/id1499140700"
+            >
+              <figure>
+                <span>
+                  <img
+                    className="platform-link-styles"
+                    src="https://fluxconsole.com/files/image/189424?width=375"
+                    alt="Listen on Apple Podcasts"
+                  />
+                </span>
+              </figure>
+            </a>
+          </div>
+        </div>
+      </section>
+      <section className="fire"></section>
     </>
-  )
-}
+  );
+};
 export default Home;
