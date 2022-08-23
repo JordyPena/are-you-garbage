dd62d6edb89a576d44edb05c3c567403706e966a
Jordy Pena <jordypenamontes@gmail.com>
2022-08-23 16:41:42 -0500

    footer complete

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,32 +1,37 @@
-import { AiFillInstagram, AiOutlineTwitter, AiFillMail } from 'react-icons/ai';
-import '../footer/Footer.css';
+import { AiFillInstagram, AiOutlineTwitter, AiFillMail } from "react-icons/ai";
+import "../footer/Footer.css";
 const Footer = () => {
   return (
     <footer>
-        <section className='footer-link-container'>
-        <a href='https://www.instagram.com/areyougarbage/' target='_blank'>
-          <AiFillInstagram className='footer-icons'/>
-          <p className='footer-text'>@areyougarbage</p>
+      <div className="media-container">
+        <a
+          href="https://twitter.com/AreYouGarbage?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"
+          target="_blank"
+        >
+          <AiOutlineTwitter className="media-icons" />
         </a>
-        <a href='https://www.instagram.com/hfoleycomedy/' target='_blank'>
-          <AiFillInstagram className='footer-icons'/>
-          <p className='footer-text'>@hfoleycomedy</p>
+        <a href="mailto:areyougarbage@gmail.com">
+          <AiFillMail className="media-icons" />
         </a>
-        <a href='https://www.instagram.com/kevinryancomedy/' target='_blank'>
-          <AiFillInstagram className='footer-icons'/>
-          <p className='footer-text'>@kevinryancomedy</p>
+      </div>
+      <section className="footer-link-container">
+        <a href="https://www.instagram.com/areyougarbage/" target="_blank">
+          <AiFillInstagram className="footer-icons" />
+          <p className="footer-text">@areyougarbage</p>
         </a>
-        <a href='https://twitter.com/AreYouGarbage?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor' target='_blank'>
-          <AiOutlineTwitter className='footer-icons'/>
+        <a href="https://www.instagram.com/hfoleycomedy/" target="_blank">
+          <AiFillInstagram className="footer-icons" />
+          <p className="footer-text">@hfoleycomedy</p>
         </a>
-        <a href='mailto:areyougarbage@gmail.com'>
-          <AiFillMail className='footer-icons'/>
+        <a href="https://www.instagram.com/kevinryancomedy/" target="_blank">
+          <AiFillInstagram className="footer-icons" />
+          <p className="footer-text">@kevinryancomedy</p>
         </a>
       </section>
-      <div className='copyright-container'>
-        <p className='copyright-styles'>&copy;2022 by AreYouGarbage</p>
+      <div className="copyright-container">
+        <p className="copyright-styles">&copy;2022 by AreYouGarbage</p>
       </div>
     </footer>
-  )
-}
+  );
+};
 export default Footer;
